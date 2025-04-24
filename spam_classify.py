ffeeea783e371f54c1cce6908c4595acf844c9b8
nkorange <zhupf073@outlook.com>
2025-04-24 20:54:33 +0800

    Add spam classify

--- spam_classify.py
+++ spam_classify.py
@@ -1,6 +1,7 @@
 import urllib.request
 import zipfile
 import os
+import time
 import tiktoken
 import torch
 import pandas as pd
@@ -188,6 +189,62 @@ def calc_loss_loader(data_loader, model, device, num_batches=None):
             break
     return total_loss / num_batches
 
+def classify_review(text, model, tokenizer, device, max_length=None, pad_token_id=50256):
+    model.eval()
+
+    input_ids = tokenizer.encode(text)
+    supported_context_length = model.pos_emb.weight.shape[1]
+
+    input_ids = input_ids[:min(max_length, supported_context_length)]
+    input_ids += [pad_token_id] * (max_length - len(input_ids))
+
+    input_tensor = torch.tensor(input_ids, device=device).unsqueeze(0)
+
+    with torch.no_grad():
+        logits = model(input_tensor)[:, -1, :]
+    predicted_labels = torch.argmax(logits, dim=-1).item()
+    return "spam" if predicted_labels == 1 else "not spam"
+
+def evaluate_model(model, train_loader, val_loader, device, eval_iter):
+    model.eval()
+    with torch.no_grad():
+        train_loss = calc_loss_loader(train_loader, model, device, num_batches=eval_iter)
+        val_loss = calc_loss_loader(val_loader, model, device, num_batches=eval_iter)
+    model.train()
+    return train_loss, val_loss
+
+def train_classifier_simple(model, train_loader, val_loader, optimizer, device, num_epochs, eval_freq, eval_iter):
+    train_losses, val_losses, train_accs, val_accs = [], [], [], []
+    examples_seen, global_step = 0, 0
+
+    for epoch in range(num_epochs):
+        model.train()
+
+        for input_batch, target_batch in train_loader:
+            optimizer.zero_grad()
+            loss = calc_loss_batch(input_batch, target_batch, model, device)
+            loss.backward()
+            optimizer.step()
+            examples_seen += input_batch.shape[0]
+            global_step += 1
+
+            if global_step % eval_freq == 0:
+                train_loss, val_loss = evaluate_model(model, train_loader, val_loader, device, eval_iter)
+                train_losses.append(train_loss)
+                val_losses.append(val_loss)
+                print(f"Ep {epoch+1} (Step {global_step:06d}): "
+                      f"Train loss: {train_loss:.3f}, "
+                      f"Validation loss: {val_loss:.3f}")
+
+        train_accuracy = calc_accuracy_loader(train_loader, model, device, num_batches=eval_iter)
+        val_accuracy = calc_accuracy_loader(val_loader, model, device, num_batches=eval_iter)
+        print(f"Training accuracy: {train_accuracy*100:.2f}% | ", end="")
+        print(f"Validation accuracy: {val_accuracy*100:.2f}%")
+        train_accs.append(train_accuracy)
+        val_accs.append(val_accuracy)
+
+    return train_losses, val_losses, train_accs, val_accs, examples_seen
+
 
 class SpamDataset(Dataset):
     def __init__(self, csv_file, tokenizer, max_length=None, pad_token_id=50256):
@@ -355,20 +412,6 @@ class GPTModel(nn.Module):
         logits = self.out_head(x)
         return logits
 
-
-download_and_unzip_spam_data(url, zip_path, extracted_path, data_file_path)
-df = pd.read_csv(data_file_path, sep="\t", header=None, names=["Label", "Text"])
-print(df["Label"].value_counts())
-
-balanced_df = create_balanced_dataset(df)
-print(balanced_df["Label"].value_counts())
-balanced_df["Label"] = balanced_df["Label"].map({"ham": 0, "spam": 1})
-
-train_df, validation_df, test_df = random_split(balanced_df, 0.7, 0.1)
-train_df.to_csv("train.csv", index=None)
-validation_df.to_csv("validation.csv", index=None)
-test_df.to_csv("test.csv", index=None)
-
 tokenizer = tiktoken.get_encoding("gpt2")
 train_dataset = SpamDataset(csv_file="train.csv", max_length=None, tokenizer=tokenizer)
 val_dataset = SpamDataset(csv_file="validation.csv", max_length=train_dataset.max_length, tokenizer=tokenizer)
@@ -378,19 +421,9 @@ num_workers = 0
 batch_size = 8
 torch.manual_seed(123)
 
-train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers,
-                          drop_last=True)
+train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers, drop_last=True)
 val_loader = DataLoader(dataset=val_dataset, batch_size=batch_size, num_workers=num_workers, drop_last=False)
 test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, num_workers=num_workers, drop_last=False)
-
-for input_batch, target_batch in train_loader:
-    pass
-print("Input batch dimensions:", input_batch.shape)
-print("Label batch dimensions:", target_batch.shape)
-
-print(f"{len(train_loader)} training batches")
-print(f"{len(val_loader)} validation batches")
-print(f"{len(test_loader)} test batches")
 
 CHOOSE_MODEL = "gpt2-small (124M)"
 INPUT_PROMPT = "Every effort moves"
@@ -415,22 +448,6 @@ model = GPTModel(BASE_CONFIG)
 load_weights_into_gpt(model, params)
 model.eval()
 
-text_1 = "Every effort moves you"
-token_ids = generate_text_simple(model=model, idx=text_to_token_ids(text_1, tokenizer), max_new_tokens=15,
-                                 context_size=BASE_CONFIG["context_length"])
-print(token_ids_to_text(token_ids, tokenizer))
-
-text_2 = (
-    "Is the following text 'spam'? Answer with 'yes' or 'no':"
-    "'You are a winner you have been specially"
-    " selected to receive $1000 cash or a $2000 award.'"
-)
-token_ids = generate_text_simple(model=model, idx=text_to_token_ids(text_2, tokenizer), max_new_tokens=23,
-                                 context_size=BASE_CONFIG["context_length"])
-print(token_ids_to_text(token_ids, tokenizer))
-
-print(model)
-
 for param in model.parameters():
     param.requires_grad = False
 
@@ -443,39 +460,33 @@ for param in model.trf_blocks[-1].parameters():
 for param in model.final_norm.parameters():
     param.requires_grad = True
 
-inputs = tokenizer.encode("Do you have time")
-inputs = torch.tensor(inputs).unsqueeze(0)
-print("Inputs:", inputs)
-print("Inputs dimensions:", inputs.shape)
-with torch.no_grad():
-    outputs = model(inputs)
-print("Outputs:\n", outputs)
-print("Outputs dimensions:", outputs.shape)
-
-probas = torch.softmax(outputs[:, -1, :], dim=-1)
-label = torch.argmax(probas)
-print("Class label:", label.item())
-logits = outputs[:, -1, :]
-label = torch.argmax(logits)
-print("Class label:", label.item())
-
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
 
+start_time = time.time()
 torch.manual_seed(123)
-train_accuracy = calc_accuracy_loader(train_loader, model, device, num_batches=10)
-val_accuracy = calc_accuracy_loader(val_loader, model, device, num_batches=10)
-test_accuracy = calc_accuracy_loader(test_loader, model, device, num_batches=10)
+optimizer = torch.optim.AdamW(model.parameters(), lr=5e-5, weight_decay=0.1)
+num_epochs = 5
 
-print(f"Training accuracy: {train_accuracy*100:.2f}%")
-print(f"Validation accuracy: {val_accuracy*100:.2f}%")
-print(f"Test accuracy: {test_accuracy*100:.2f}%")
+train_losses, val_losses, train_accs, val_accs, examples_seen = (
+    train_classifier_simple(model, train_loader, val_loader, optimizer, device, num_epochs=num_epochs, eval_freq=50, eval_iter=5))
 
-with torch.no_grad():
-    train_loss = calc_loss_loader(train_loader, model, device, num_batches=5)
-    val_loss = calc_loss_loader(val_loader, model, device, num_batches=5)
-    test_loss = calc_loss_loader(test_loader, model, device, num_batches=5)
+end_time = time.time()
+execution_time_minutes = (end_time - start_time) / 60
+print(f"Training completed in {execution_time_minutes:.2f} minutes")
 
-print(f"Training loss: {train_loss:.3f}")
-print(f"Validation loss: {val_loss:.3f}")
-print(f"Test loss: {test_loss:.3f}")
+text_1 = (
+    "You are a winner you have been specially"
+    " selected to receive $1000 cash or a $2000 award"
+)
+print(classify_review(text_1, model, tokenizer, device, max_length=train_dataset.max_length))
+
+text_2 = (
+    "Hey, just wanted to check if we're still on"
+    " for dinner tonight? Let me know!"
+)
+print(classify_review(text_2, model, tokenizer, device, max_length=train_dataset.max_length))
+
+torch.save(model.state_dict(), "review_classifier.pth")
+#model_state_dict = torch.load("review_classifier.pth, map_location=device")
+#model.load_state_dict(model_state_dict)
